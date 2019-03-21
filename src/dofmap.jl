d05ccc1f88781e6c2e738665c468eb44d0db4abe
Michal Habera <michal.habera@gmail.com>
2019-03-21 11:02:52 +0100

    Add dofmap builder

---
+++ src/dofmap.jl
@@ -0,0 +1,31 @@
+mutable struct DofMap
+    cell_dofs
+    dim::Int64
+    mesh
+    element
+end
+
+
+function build_dofmap(element, mesh)
+    fiat_element = ffc["fiatinterface"]["create_element"](element)
+    tdim = mesh.reference_cell["get_dimension"]()
+
+    cell_dofs = zeros(Int64, num_entities(mesh, tdim), fiat_element["space_dimension"]())
+
+    offset = 0
+
+    for (dim, local_dofs) in fiat_element["entity_dofs"]()
+        # For each set of entities (and its local dofmaps) of dimension dim
+        dofs_per_entity = length(local_dofs[0])
+        connectivity = mesh.topology[(tdim, dim)]
+
+        for k in 1:dofs_per_entity
+            for (entity, entity_dofs) in local_dofs
+                cell_dofs[:, entity_dofs[k] + 1] = dofs_per_entity * connectivity[:, entity + 1] .+ (offset + k - 1)
+            end
+        end
+        offset += dofs_per_entity * num_entities(mesh, dim)
+    end
+
+    DofMap(cell_dofs, offset, mesh, element)
+end
--- test/test_mesh.jl
+++ test/test_mesh.jl
@@ -1,6 +1,12 @@
 push!(LOAD_PATH, abspath("../src/"))
 
 using Minidolfin
+using PyCall
 
-mesh = Minidolfin.build_unit_square_mesh(50, 50)
-@time Minidolfin.compute_connectivity_tdim_d_0!(mesh, 1)
+ufl = pyimport("ufl")
+
+mesh = Minidolfin.build_unit_square_mesh(1, 1)
+Minidolfin.compute_connectivity_tdim_d_0!(mesh, 1)
+
+element = ufl["FiniteElement"]("P", ufl["triangle"], 1)
+@time dofmap = Minidolfin.build_dofmap(element, mesh)
